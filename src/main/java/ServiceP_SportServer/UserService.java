9de42c511dde707da70ca66604350077d23b0d14
Johannes Eifert <johannes.eifert@unifr.ch>
2012-10-25 15:13:02 +0200

    test

---
+++ src/main/java/ServiceP_SportServer/UserService.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ServiceP_SportServer;
+
+/**
+ *
+ * @author Johannes Eifert
+ */
+public class UserService {
+    
+}
