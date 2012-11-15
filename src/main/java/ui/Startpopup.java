7a70505ab3e4fec0b9f62ec362de110ac4d3c0ea
Johannes Eifert <johannes.eifert@unifr.ch>
2012-11-15 11:36:45 +0100

    small changes

--- src/main/java/ui/Startpopup.java
+++ src/main/java/ui/Startpopup.java
@@ -5,6 +5,7 @@
 package ui;
 
 import ServiceP_SportServer.UserService;
+import com.vaadin.terminal.ExternalResource;
 import com.vaadin.terminal.ThemeResource;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
@@ -12,7 +13,7 @@ import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Window;
 
 /**
- *
+ * This Object is called on startup or when ever a user wants to change his status.
  * @author Johannes Eifert
  */
 public class Startpopup extends Window implements Button.ClickListener{
@@ -32,7 +33,9 @@ public class Startpopup extends Window implements Button.ClickListener{
     hl.addComponent(loginUserButton = new Button("Login", this));
     hl.addComponent(createUserButton = new Button("Create a new User", this));
     this.addComponent(hl);
-    loginUserButton.setIcon(new ThemeResource("icons/32/folder-add.png"));
+    //Setting the icons. Usually I would copy them locally, but here I trust in resilience of the icons online.
+    loginUserButton.setIcon(new ExternalResource("http://www.feuerwehr-ostrau.de/templates/rt_kinetic_j15/images/icons/icon-key.png"));
+    createUserButton.setIcon(new ExternalResource("http://images01.uboot.com/images/pix/icons/user--plus.png"));
     }
 
     public void buttonClick(ClickEvent e) {
